c08730086581faeb2fbc10bde099a61ed38c4fba
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-09-14 07:58:43 +0100

    update

--- app.js
+++ app.js
@@ -17,8 +17,24 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/', (req, res) => {
+const item1 = new Item({
+    name: 'Item 1'
+})
 
+const item2 = new Item({
+    name: 'Item 2'
+})
+
+const defaultItems = [item1, item2]
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+})
+
+const List = mongoose.model('List', listSchema)
+
+app.get('/', (req, res) => {
     Item.find({}, async (err, foundItems) => {
         res.render('list', { listTitle: 'Today', newItemList: foundItems })
     })
@@ -27,12 +43,22 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 
     const itemName = req.body.newItem
+    const listName = req.body.list
+
     const item = new Item({
         name: itemName
     })
-    item.save()
 
-    res.redirect('/')
+    if (listName === 'Today') {
+        item.save()
+        res.redirect('/')   
+    } else {
+        List.findOne({ name: listName }, (err, foundList) => {
+            foundList.items.push(item)
+            foundList.save()
+            res.redirect(`/${listName}`)
+        })
+    }
 
 })
 
@@ -46,6 +72,23 @@ app.post('/delete', (req, res) => {
 
 app.get('/:customListName', (req, res) => {
     const customListName = req.params.customListName
+
+    List.findOne({ name: customListName}, (err, foundList) => {
+        if(!foundList) {
+            // Creates new list
+            const list = new List({
+                name: customListName,
+                items: defaultItems
+            })
+        
+            list.save()
+            res.redirect(`/${customListName}`)
+        } else {
+            // Shows an existing list
+            res.render('list', {listTitle: foundList.name, newItemList: foundList.items})
+        }
+    })
+
 })
 
 app.get('/about', (req, res) => {
